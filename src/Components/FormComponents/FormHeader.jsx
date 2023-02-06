6c8e03fc719b1b5ce9c31c0d609a8fda5467bd7f
Raxxius <raxxman@gmail.com>
2023-02-06 10:41:15 +0000

    deconstructed props

--- src/Components/FormComponents/FormHeader.jsx
+++ src/Components/FormComponents/FormHeader.jsx
@@ -1,19 +1,51 @@
 import { handleSubmit } from "../../Functions/formFunctions";
 
 export default function FormHeader(props) {
+  /** props deconstruction */
+  const {
+    formIntro,
+    pageNumber,
+    numberOfPages,
+    handlePageChange,
+    setPageNumber,
+    pages,
+  } = props;
+
   return (
     <div className="form-footer">
-      <div className="form-intro">
-            Form intro go here
-      </div>
-      <div className="submit-button" onClick={() => handleSubmit(props.pages)}>
-            Form submit button
+      <div className="form-intro">{formIntro}</div>
+      <div className="submit-button" onClick={() => handleSubmit(pages)}>
+        Form submit button
       </div>
       <div className="page-selector">
-          Page selector
-          <div>{props.pageNumber} of {props.numberOfPages}</div>
-          <div className="prev-page" onClick={() => props.handlePageChange("prev", props.setPageNumber, props.numberOfPages)}>Prev Page</div>
-          <div className="next-page" onClick={() => props.handlePageChange("next", props.setPageNumber, props.numberOfPages)}>Next Page</div>
+        Page selector
+        <div>
+          {pageNumber} of {numberOfPages}
+        </div>
+        <div
+          className="prev-page"
+          onClick={() =>
+            props.handlePageChange(
+              "prev",
+              setPageNumber,
+              numberOfPages
+            )
+          }
+        >
+          Prev Page
+        </div>
+        <div
+          className="next-page"
+          onClick={() =>
+            handlePageChange(
+              "next",
+              setPageNumber,
+              numberOfPages
+            )
+          }
+        >
+          Next Page
+        </div>
       </div>
     </div>
   );
