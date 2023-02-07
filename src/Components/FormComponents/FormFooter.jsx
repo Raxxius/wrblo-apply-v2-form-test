959e3fe427cbc34534b654d1da75aba87297cbab
Raxxius <raxxman@gmail.com>
2023-02-07 16:14:41 +0000

    added footer styling

--- src/Components/FormComponents/FormFooter.jsx
+++ src/Components/FormComponents/FormFooter.jsx
@@ -6,25 +6,9 @@ export default function FormFooter(props) {
     <div className="form-footer">
       <div
         className="save-load-button"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "80vw",
-          marginRight: "auto",
-          marginLeft: "auto",
-          marginBottom: "20px",
-        }}
       >
         <div
-          className="save-button"
-          style={{
-            backgroundColor: "white",
-            cursor: "pointer",
-            border: "2px solid black",
-            padding: "5px",
-            marginTop: "15px",
-            marginLeft: "15px",
-          }}
+          className="load-button"
         >
           <input
             type="file"
@@ -33,21 +17,12 @@ export default function FormFooter(props) {
             onChange={(e) => handleLoad(e, setFormData)}
           />
         </div>
-        <div
+        <button
           className="save-button"
-          style={{
-            cursor: "pointer",
-            color: "white",
-            backgroundColor: "darkblue",
-            width: "150px",
-            padding: "5px",
-            marginTop: "15px",
-            marginRight: "0",
-          }}
           onClick={() => handleSave(formData)}
         >
           Save Form
-        </div>
+        </button>
       </div>
     </div>
   );
