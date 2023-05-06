4042f9114c2d5df7e2aa945c2ae5c77d99332d75
Keith Blackwood <raxxman@gmail.com>
2023-05-06 14:26:32 +0100

    removed import of data (unused)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Form from './Components/Form'
-import Example from './Tests/Example2';
 import '../src/App.css'
 
 function App() {
