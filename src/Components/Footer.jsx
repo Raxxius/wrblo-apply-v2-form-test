c2259c137165e444762607e137df7da6790e35b5
Raxxius <raxxman@gmail.com>
2023-10-10 20:09:59 +0100

    added footer and footer tail content+styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,31 @@
+const AboutUs = () => {
+  return <div className="footer-about">About Us</div>;
+};
 
+const Tail = () => {
+  return (
+    <div className="footer-tail">
+      <div className="footer-links">
+        <a href="">Code of Conduct</a>&nbsp; &nbsp;&nbsp;
+        <a href="https://wrblo.org/privacy-policy/">Privacy Policy</a>
+      </div>
+      <div className="footer-text">
+        <span>&copy;</span>2023 All rights reserved
+      </div>
+      <div className="footer-text">
+        <a href="">Trademarks</a>
+      </div>
+    </div>
+  );
+};
 
 export default function Footer() {
-    return (
-        <div>
-            Footer goes here
-        </div>
-    )
+  return (
+    <footer>
+      <div className="footer-inner">
+        <AboutUs />
+        <Tail />
+      </div>
+    </footer>
+  );
 }
