438e37ce96158b315c72e3772540222103a4d7b0
Raxxius <raxxman@gmail.com>
2023-10-14 19:55:05 +0100

    add header content

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,11 @@
-export default function Form() {
-  return <div className="main-header">WRBLO Preliminary Application</div>;
+import logo from "../assets/WRBLO_logo-removebg-preview.png" 
+
+export default function Header() {
+  return (
+  <div className="main-header">
+    <img src={logo} alt="WRLBO logo" className="header-image"/>
+    <h1 className="header-title">Quick Apply</h1>
+    <a href="www.wrblo.org" className="header-links">Go back &nbsp;&nbsp;{'\u2192'}</a>
+  </div>
+  )
 }
