e5e723d40fb95753f20312905eadc65902625679
Raxxius <raxxman@gmail.com>
2023-10-14 20:02:51 +0100

    complete header styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,15 @@
-import logo from "../assets/WRBLO_logo-removebg-preview.png" 
+import logo from "../assets/WRBLO_logo-removebg-preview.png";
 
 export default function Header() {
   return (
-  <div className="main-header">
-    <img src={logo} alt="WRLBO logo" className="header-image"/>
-    <h1 className="header-title">Quick Apply</h1>
-    <a href="www.wrblo.org" className="header-links">Go back &nbsp;&nbsp;{'\u2192'}</a>
-  </div>
-  )
+    <div className="main-header">
+      <div className="header-wrapper">
+        <img src={logo} alt="WRLBO logo" className="header-image" />
+        <h1 className="header-title">Quick Apply</h1>
+        <a href="www.wrblo.org" className="header-links">
+          Back to WRBLO website &nbsp;&nbsp;{"\u2192"}
+        </a>
+      </div>
+    </div>
+  );
 }
